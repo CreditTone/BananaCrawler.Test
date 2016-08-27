6fc535a12c7119175be0e24508a8fb7295cdae1d
zhongguo <zhongguo@creditease.cn>
2016-08-27 11:55:55 +0800

    test processor

--- src/main/java/banana/test/main/TestTask.java
+++ src/main/java/banana/test/main/TestTask.java
@@ -2,6 +2,10 @@ package banana.test.main;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,55 +21,79 @@ import org.apache.hadoop.ipc.RPC;
 import org.apache.hadoop.ipc.RPC.Server;
 
 import com.alibaba.fastjson.JSON;
-import com.banana.master.impl.CrawlerMasterServer;
 
+import banana.core.download.impl.DefaultPageDownloader;
 import banana.core.modle.CrawlData;
 import banana.core.processor.DataProcessor;
+import banana.core.processor.PageProcessor;
 import banana.core.protocol.CrawlerMasterProtocol;
 import banana.core.protocol.DownloadProtocol;
+import banana.core.protocol.Extractor;
 import banana.core.protocol.Task;
+import banana.core.request.HttpRequest;
+import banana.core.request.StartContext;
+import banana.core.response.Page;
 import banana.crawler.dowload.impl.DownloadServer;
+import banana.crawler.dowload.impl.JsonRpcExtractor;
+import banana.crawler.dowload.processor.JSONConfigPageProcessor;
+import banana.master.impl.CrawlerMasterServer;
+import banana.master.task.TaskTracker;
 
 public class TestTask {
-	
+
 	static CrawlerMasterServer crawlerMasterServer;
 	static Server masterServer;
 	static Server downloadServer;
 
 	public static void main(String[] args) throws Exception {
-		CommandLineParser parser = new DefaultParser();  
+		CommandLineParser parser = new DefaultParser();
 		Options options = new Options();
 		options.addOption("e", "extractor", true, "Set the extractor host");
-		options.addOption("t", "test", true, "test task from a jsonfile");
-		CommandLine commandLine = parser.parse(options, args); 
-		if (!commandLine.hasOption("e") ||!commandLine.hasOption("t") ){
+		options.addOption("t", "test", true, "Test task from a jsonfile");
+		options.addOption("p", "processor", true, "Test the target processor");
+		CommandLine commandLine = parser.parse(options, args);
+		if (!commandLine.hasOption("e") || !commandLine.hasOption("t")) {
 			System.out.println("Must have extractor configuration");
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("Test", options);
 			return;
 		}
-		if (commandLine.hasOption("t")){
-			String taskFilePath = commandLine.getOptionValue('t');
-			Task task = initOneTask(taskFilePath);
-			task.verify();
-			String extractorAddress = commandLine.getOptionValue("e");
-			TestTask.start( task, extractorAddress);
-			return;
+		String taskFilePath = commandLine.getOptionValue('t');
+		Task task = initOneTask(taskFilePath);
+		task.verify();
+		String extractorAddress = commandLine.getOptionValue("e");
+		if (commandLine.hasOption("p")){
+			String[] split = commandLine.getOptionValue('p').split(",");
+			String processorIndex = split[0];
+			String url = split[1];
+			String taskid = task.name + "_" + new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+			Task.Processor processor = null;
+			for (Task.Processor p : task.processors) {
+				if(p.getIndex().equals(processorIndex)){
+					processor = p;
+					break;
+				}
+			}
+			Extractor extractor = new JsonRpcExtractor(extractorAddress);
+			JSONConfigPageProcessor pro = new JSONConfigPageProcessor(taskid, processor, extractor);
+			testProcessor(pro, url);
+		}else{
+			TestTask.start(task, extractorAddress);
 		}
 	}
-	
-	public static void start(Task task,String extractorAddress) throws Exception {
+
+	public static void start(Task task, String extractorAddress) throws Exception {
 		startTestMaster(extractorAddress);
 		startTestDownloader();
 		System.out.println("To begin testing");
 		task.thread = 1;
-		if (task.queue == null){
+		if (task.queue == null) {
 			task.queue = new HashMap<String, Object>();
 		}
 		task.queue.put("delay", 1000);
 		crawlerMasterServer.submitTask(task);
-		while(true){
-			if (crawlerMasterServer.existTask(task.name).get()){
+		while (true) {
+			if (crawlerMasterServer.existTask(task.name).get()) {
 				Thread.sleep(100);
 				continue;
 			}
@@ -78,35 +106,54 @@ public class TestTask {
 	private static void startTestDownloader() throws Exception {
 		DownloadServer downloadServer = DownloadServer.initInstance("localhost:9666");
 		downloadServer.dataProcessor = new DataProcessor() {
-			
+
 			public void process(List<CrawlData> objectContainer, String... args) throws Exception {
-				for (CrawlData data : objectContainer){
+				for (CrawlData data : objectContainer) {
 					System.out.println(data.getData().toMap());
 				}
 			}
 		};
-		if (downloadServer != null){
+		if (downloadServer != null) {
 			TestTask.downloadServer = new RPC.Builder(new Configuration()).setProtocol(DownloadProtocol.class)
-	                .setInstance(downloadServer).setBindAddress("localhost").setPort(9777)
-	                .setNumHandlers(5).build();
+					.setInstance(downloadServer).setBindAddress("localhost").setPort(9777).setNumHandlers(5).build();
 			TestTask.downloadServer.start();
-	        downloadServer.getMasterServer().registerDownloadNode("localhost",9777);
-	        System.out.println("Test Downloader started");
+			downloadServer.getMasterServer().registerDownloadNode("localhost", 9777);
+			System.out.println("Test Downloader started");
 		}
 	}
 
-	private static void startTestMaster(String extractorAddress) throws HadoopIllegalArgumentException, IOException{
+	private static void startTestMaster(String extractorAddress) throws HadoopIllegalArgumentException, IOException {
 		crawlerMasterServer = new CrawlerMasterServer();
-		if (crawlerMasterServer != null){
+		if (crawlerMasterServer != null) {
 			crawlerMasterServer.setMasterPropertie("EXTRACTOR", extractorAddress);
 			TestTask.masterServer = new RPC.Builder(new Configuration()).setProtocol(CrawlerMasterProtocol.class)
-	                .setInstance(crawlerMasterServer).setBindAddress("localhost").setPort(9666)
-	                .setNumHandlers(5).build();
+					.setInstance(crawlerMasterServer).setBindAddress("localhost").setPort(9666).setNumHandlers(5)
+					.build();
+			TaskTracker.MODE = TaskTracker.TEST_MODE;
 			TestTask.masterServer.start();
 			System.out.println("Test Master started");
 		}
 	}
 	
+	private static void testProcessor(PageProcessor pro,String url) throws Exception{
+		StartContext startContext = new StartContext();
+		DefaultPageDownloader downloader = new DefaultPageDownloader();
+		downloader.open();
+		Page page = downloader.go(startContext.createPageRequest(url, ""));
+		List<HttpRequest> queue = new ArrayList<HttpRequest>();
+		List<CrawlData> objectContainer = new ArrayList<CrawlData>();
+		pro.process(page, startContext, queue, objectContainer);
+		System.out.println("extract request:" + queue.size());
+		for (HttpRequest request : queue) {
+			System.out.println(request.getUrl());
+		}
+		System.out.println("extract crawler_data:" + objectContainer.size());
+		for (CrawlData data : objectContainer) {
+			System.out.println(data.getData().toMap());
+		}
+		downloader.close();
+	}
+
 	private static Task initOneTask(String taskFilePath) throws IOException {
 		File file = new File(taskFilePath);
 		String json = FileUtils.readFileToString(file, "utf-8");
@@ -114,4 +161,5 @@ public class TestTask {
 		task.data = json;
 		return task;
 	}
+
 }
